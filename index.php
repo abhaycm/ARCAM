be3f9f144fed014e4e0ef8c2c6152a4e2fe08b30
athulraimon <athulraimon@gmail.com>
2023-11-09 12:14:09 +0530

    elvish bhai

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-    //session_destroy();
+    
 
     include("includes/config.php");
+    //session_destroy();
     if(isset($_SESSION['userLoggedIn'])){
         $userLoggedIn = $_SESSION['userLoggedIn'];
     }
@@ -14,10 +15,129 @@
 <html>
     <head>
         <title>
-
+        Welcome to ARCAM
         </title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     </head>
     <body>
-        Hello!
+        <div id="mainContainer">
+
+            <div id="topContainer">
+                <div id="navBarContainer">
+                    <nav class="navBar">
+
+                        <a href="index.php" class="logo">
+                            <img src="assets/images/icons/logo.png">
+                        </a>
+                        <div class="group">
+
+                            <div class="navItem">
+                                <a href="search.php" class="navItemLink">Search
+                                    <img src="assets/images/icons/search.png" class="icon" alt="Search">
+                                </a>
+                            </div>
+
+					    </div>
+
+                        <div class="group">
+                            <div class="navItem">
+                                <a href="browse.php" class="navItemLink">Browse</a>
+                            </div>
+
+                            <div class="navItem">
+                                <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                            </div>
+
+                            <div class="navItem">
+                                <a href="profile.php" class="navItemLink">Reece Kenney</a>
+                            </div>
+                        </div>
+
+
+                    </nav>
+                </div>
+                
+                
+            </div>
+
+            <div id="nowPlayingBarContainer">
+                <div id="nowPlayingBar">
+                    <div id="nowPlayingLeft">
+                        <div class="content">
+                            <span class="albumLink">
+                                <img src="https://freesvg.org/img/1286146771.png" class="albumArtwork" alt="">
+                            </span>
+
+                            <div class="trackInfo">
+
+                                <span class="trackName">
+                                    <span>Happy Birthday</span>
+                                </span>
+
+                                <span class="artistName">
+                                    <span>Reece Kenney</span>
+                                </span>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="nowPlayingCenter">
+                        <div class="content playerControls">
+                            <div class="buttons">
+                                <button class="controlButton shuffle" title="Shuffle Button">
+                                    <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                                </button>
+                                <button class="controlButton previous" title="Previous Button">
+                                    <img src="assets/images/icons/previous.png" alt="Previous">
+                                </button>
+                                <button class="controlButton play" title="Play Button">
+                                    <img src="assets/images/icons/play.png" alt="Play">
+                                </button>
+                                <button class="controlButton pause" title="Pause Button" style="display:none;">
+                                    <img src="assets/images/icons/pause.png" alt="Pause">
+                                </button>
+                                <button class="controlButton next" title="Next Button">
+                                    <img src="assets/images/icons/next.png" alt="Next">
+                                </button>
+                                <button class="controlButton repeat" title="Repeat Button">
+                                    <img src="assets/images/icons/repeat.png" alt="Repeat">
+                                </button>
+                            </div>
+
+                            <div class="playbackBar">
+
+                                <span class="progressTime current">0.00</span>
+                                <div class="progressBar">
+                                    <div class="progressBarBg">
+                                        <div class="progress"></div>
+                                    </div>
+                                </div>
+                                <span class="progressTime remaining">0.00</span>
+                        
+                            </div>
+                        </div>
+                    </div>  
+                        
+                    <div id="nowPlayingRight">
+                        <div class="volumeBar">
+
+                            <button class="controlButton volume" title="Volume button">
+                                <img src="assets/images/icons/volume.png" alt="Volume">
+                            </button>
+
+                            <div class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </body>
 </html>
--- register.php
+++ register.php
@@ -18,66 +18,109 @@
 <html>
 <head>
 	<title>Welcome to ARCAM!</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="assets/js/register.js"></script>
 </head>
 <body>
+	<?php
 
-	<div id="inputContainer">
-		<form id="loginForm" action="register.php" method="POST">
-			<h2>Login to your account</h2>
-			<p>
-				<?php echo $account->getError(Constants::$loginFailed); ?>
-				<label for="loginUsername">Username</label>
-				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
-			</p>
-			<p>
-				<label for="loginPassword">Password</label>
-				<input id="loginPassword" name="loginPassword" type="password" placeholder="your password" required>
-			</p>
+	if(isset($_POST['registerButton'])) {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").hide();
+					$("#registerForm").show();
+				});
+			</script>';
+	}
+	else {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").show();
+					$("#registerForm").hide();
+				});
+			</script>';
+	}
 
-			<button type="submit" name="loginButton">LOG IN</button>
-			
-		</form>
+	?>
+	<div id="background">
+		<div id="loginContainer">
+			<div id="inputContainer">
+				<form id="loginForm" action="register.php" method="POST">
+					<h2>Login to your account</h2>
+					<p>
+						<?php echo $account->getError(Constants::$loginFailed); ?>
+						<label for="loginUsername">Username</label>
+						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('loginUsername') ?>" required>
+					</p>
+					<p>
+						<label for="loginPassword">Password</label>
+						<input id="loginPassword" name="loginPassword" type="password" placeholder="your password" required>
+					</p>
 
-		<form id="registerForm" action="register.php" method="POST">
-			<h2>Create your account</h2>
-			<p>
-				<?php echo $account->getError(Constants::$usernameCharacters); ?>
-				<?php echo $account->getError(Constants::$usernameTaken); ?>
-				<label for="username">Username</label>
-				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
-			</p>
-			<p>
-				<?php echo $account->getError(Constants::$nameCharacters); ?>
-				<label for="name">Name</label>
-				<input id="name" name="name" type="text" placeholder="e.g. bart" value="<?php getInputValue('name') ?>" required>
-			</p>
-			<p>
-				<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-				<?php echo $account->getError(Constants::$emailInvalid); ?>
-				<?php echo $account->getError(Constants::$emailTaken); ?>
-				<label for="email">Email</label>
-				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
-			</p>
+					<button type="submit" name="loginButton">LOG IN</button>
 
-			<p>
-				<label for="email2">Confirm Email</label>
-				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
-			</p>
-			<p>
-				<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
-				<?php echo $account->getError(Constants::$passwordCharacters); ?>
-				<label for="password">Password</label>
-				<input id="password" name="password" type="password" placeholder="your password"  required>
-			</p> 
-			<p>
-				<label for="password2">Confirm Password</label>
-				<input id="password2" name="password2" type="password" placeholder="your password" required>
-			</p>
+					<div classs="hasAccountText">
+						<span id="hideLogin" style="cursor: pointer">Don't have an account yet? Signup here.</span>
+					</div>		
+					
+				</form>
 
-			<button type="submit" name="registerButton">SIGN UP</button>
-			
-		</form>
+				<form id="registerForm" action="register.php" method="POST">
+					<h2>Create your account</h2>
+					<p>
+						<?php echo $account->getError(Constants::$usernameCharacters); ?>
+						<?php echo $account->getError(Constants::$usernameTaken); ?>
+						<label for="username">Username</label>
+						<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
+					</p>
+					<p>
+						<?php echo $account->getError(Constants::$nameCharacters); ?>
+						<label for="name">Name</label>
+						<input id="name" name="name" type="text" placeholder="e.g. bart" value="<?php getInputValue('name') ?>" required>
+					</p>
+					<p>
+						<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+						<?php echo $account->getError(Constants::$emailInvalid); ?>
+						<?php echo $account->getError(Constants::$emailTaken); ?>
+						<label for="email">Email</label>
+						<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
+					</p>
+
+					<p>
+						<label for="email2">Confirm Email</label>
+						<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
+					</p>
+					<p>
+						<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+						<?php echo $account->getError(Constants::$passwordCharacters); ?>
+						<label for="password">Password</label>
+						<input id="password" name="password" type="password" placeholder="your password"  required>
+					</p> 
+					<p>
+						<label for="password2">Confirm Password</label>
+						<input id="password2" name="password2" type="password" placeholder="your password" required>
+					</p>
+
+					<button type="submit" name="registerButton">SIGN UP</button>
+					<div classs="hasAccountText">
+						<span id="hideRegister" style="cursor: pointer">Already have an account? Log in here.</span>
+					</div>	
+					
+				</form>
+			</div>
+
+			<div id="loginText">
+				<h1>Get great music, right now</h1>
+				<h2>Listen to loads of songs for free</h2>
+				<ul>
+					<li>Discover music you'll fall in love with</li>
+					<li>Create your own playlists</li>
+					<li>Follow artists to keep up to date</li>
+				</ul>
+			</div>
+
+		</div>
 	</div>
-
 </body>
 </html>
